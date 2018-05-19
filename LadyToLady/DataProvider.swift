32339b550f4dbd5cdebc50dde44e6166e434e435
Dionne <dionne.condorfarrell@tfl.gov.uk>
2018-05-19 15:33:01 +0100

    iOSApp Workshop Project

---
+++ LadyToLady/DataProvider.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  LadyToLady
+//
+//  Created by Dionne Condor-Farrell on 19/05/2018.
+//  Copyright © 2018 Dionne Condor-Farrell. All rights reserved.
+//
+
+import Foundation
---
+++ LadyToLadyTests/LadyToLadyTests.swift
@@ -0,0 +1,36 @@
+//
+//  LadyToLadyTests.swift
+//  LadyToLadyTests
+//
+//  Created by Dionne Condor-Farrell on 19/05/2018.
+//  Copyright © 2018 Dionne Condor-Farrell. All rights reserved.
+//
+
+import XCTest
+@testable import LadyToLady
+
+class LadyToLadyTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
